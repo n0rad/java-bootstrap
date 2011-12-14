facf859853bc9d83113b7ea8f5ab3aeefde8a1c4
n0rad <dev@norad.fr>
2011-12-14 12:52:10 +0100

    add sample for jar

--- sample/jar-sample/src/main/java/net/awired/boostrap/sample/jar/Main.java
+++ sample/jar-sample/src/main/java/net/awired/boostrap/sample/jar/Main.java
@@ -1,9 +1,32 @@
 package net.awired.boostrap.sample.jar;
 
+import net.awired.aclm.argument.CliArgumentManager;
+import net.awired.aclm.argument.args.CliOneParamArgument;
+import net.awired.aclm.param.CliParamString;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public class ArgumentManager extends CliArgumentManager {
+        private CliOneParamArgument<String> typeArgument;
 
+        public ArgumentManager() {
+            super("sample-jar");
+
+            typeArgument = new CliOneParamArgument<String>('t', new CliParamString("type"));
+            typeArgument.setParamOneDefValue("default");
+            addArg(typeArgument);
+        }
+    }
+
+    public Main(String[] args) {
+        ArgumentManager am = new ArgumentManager();
+        am.parse(args);
+
+        System.out.println("type argument value is : " + am.typeArgument.getParamOneValue());
+    }
+
+    public static void main(String[] args) {
+        new Main(args);
     }
 
 }
